5ac5d110e0fefe4a7d445bfa5c60aa43c7f43a5b
Rob Gaston <rgaston@fargeo.com>
2022-09-24 12:39:54 -0700

    day 2 changes

--- src/process.mjs
+++ src/process.mjs
@@ -1,1 +1,26 @@
-console.log("success. 👍");
+import turf from "@turf/turf";
+import fs from "fs";
+import sites from "../data/sites.json" assert {type: "json"};
+import neighborhoods from "../data/neighborhoods.json" assert {type: "json"};
+
+sites.features.forEach(function(feature) {
+    feature.properties = {
+        count: 1
+    };
+});
+
+let output = turf.collect(neighborhoods, sites, "count", "count")
+
+output.features = output.features.filter(function(feature, index) {
+    feature.id = index;
+    feature.properties.count = feature.properties.count.length;
+    return feature.properties.count > 0;
+});
+
+output = JSON.stringify(output, null, "\t");
+
+fs.writeFile("../data/output.json", output, function(error) {
+    if (error) throw error;
+
+    console.log("success. 👍");
+});
